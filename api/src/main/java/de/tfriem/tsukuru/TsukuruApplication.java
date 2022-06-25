11bb2e39ffac6ace391e6da0d37c6a633f1a45a5
Tobias Friemel <tfriem@gmail.com>
2022-06-25 18:12:20 +0200

    Add generated spring boot project

---
+++ api/src/main/java/de/tfriem/tsukuru/TsukuruApplication.java
@@ -0,0 +1,13 @@
+package de.tfriem.tsukuru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsukuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsukuruApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/de/tfriem/tsukuru/TsukuruApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tfriem.tsukuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsukuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
